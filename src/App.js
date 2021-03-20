4210c158b1c168900a3e33db929626e2306a7737
Lucas de Azambuja <lucasazsantos@gmail.com>
2021-03-19 22:00:57 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import React from 'react';
 const App = () => {
   const [lat, setLat] = React.useState([]);
   const [long, setLong] = React.useState([]);
+  const [data, setData] = React.useState([]);
 
   React.useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
 
-    console.log('Latitude is:', lat);
-    console.log('Longitude is:', long);
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
   }, [lat, long]);
 
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <h1>OI</h1>
+    </div>
+  );
 };
+
 export default App;
